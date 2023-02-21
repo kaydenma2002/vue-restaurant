d79cc5861373b20ddb2e9f42fff650eb13a553e0
kaydenma2002 <113602175+kaydenma2002@users.noreply.github.com>
2023-02-21 14:41:02 -0800

    commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,42 +2,27 @@
   <div class="card-list">
     <div class="card-item" v-bind:class="{ '-active': isCardFlipped }">
       <div class="card-item__side -front">
-        <div
-          class="card-item__focus"
-          v-bind:class="{ '-active': focusElementStyle }"
-          v-bind:style="focusElementStyle"
-          ref="focusElement"
-        ></div>
+        <div class="card-item__focus" v-bind:class="{ '-active': focusElementStyle }" v-bind:style="focusElementStyle"
+          ref="focusElement"></div>
         <div class="card-item__cover">
-          <img
-            v-bind:src="
-              'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
-              currentCardBackground +
-              '.jpeg'
-            "
-            class="card-item__bg"
-          />
+          <img v-bind:src="
+            'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
+            currentCardBackground +
+            '.jpeg'
+          " class="card-item__bg" />
         </div>
 
         <div class="card-item__wrapper">
           <div class="card-item__top">
-            <img
-              src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
-              class="card-item__chip"
-            />
+            <img src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+              class="card-item__chip" />
             <div class="card-item__type">
               <transition name="slide-fade-up">
-                <img
-                  v-bind:src="
-                    'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
-                    getCardType +
-                    '.png'
-                  "
-                  v-if="getCardType"
-                  v-bind:key="getCardType"
-                  alt=""
-                  class="card-item__typeImg"
-                />
+                <img v-bind:src="
+                  'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
+                  getCardType +
+                  '.png'
+                " v-if="getCardType" v-bind:key="getCardType" alt="" class="card-item__typeImg" />
               </transition>
             </div>
           </div>
@@ -45,65 +30,42 @@
             <template v-if="getCardType === 'amex'">
               <span v-for="(n, $index) in amexCardMask" :key="$index">
                 <transition name="slide-fade-up">
-                  <div
-                    class="card-item__numberItem"
-                    v-if="
-                      $index > 4 &&
-                      $index < 14 &&
-                      cardNumber.length > $index &&
-                      n.trim() !== ''
-                    "
-                  >
+                  <div class="card-item__numberItem" v-if="
+                    $index > 4 &&
+                    $index < 14 &&
+                    cardNumber.length > $index &&
+                    n.trim() !== ''
+                  ">
                     *
                   </div>
-                  <div
-                    class="card-item__numberItem"
-                    :class="{ '-active': n.trim() === '' }"
-                    :key="$index"
-                    v-else-if="cardNumber.length > $index"
-                  >
+                  <div class="card-item__numberItem" :class="{ '-active': n.trim() === '' }" :key="$index"
+                    v-else-if="cardNumber.length > $index">
                     {{ cardNumber[$index] }}
                   </div>
-                  <div
-                    class="card-item__numberItem"
-                    :class="{ '-active': n.trim() === '' }"
-                    v-else
-                    :key="$index + 1"
-                  >
+                  <div class="card-item__numberItem" :class="{ '-active': n.trim() === '' }" v-else :key="$index + 1">
                     {{ n }}
                   </div>
                 </transition>
+
               </span>
             </template>
 
             <template v-else>
               <span v-for="(n, $index) in otherCardMask" :key="$index">
                 <transition name="slide-fade-up">
-                  <div
-                    class="card-item__numberItem"
-                    v-if="
-                      $index > 4 &&
-                      $index < 15 &&
-                      cardNumber.length > $index &&
-                      n.trim() !== ''
-                    "
-                  >
+                  <div class="card-item__numberItem" v-if="
+                    $index > 4 &&
+                    $index < 15 &&
+                    cardNumber.length > $index &&
+                    n.trim() !== ''
+                  ">
                     *
                   </div>
-                  <div
-                    class="card-item__numberItem"
-                    :class="{ '-active': n.trim() === '' }"
-                    :key="$index"
-                    v-else-if="cardNumber.length > $index"
-                  >
+                  <div class="card-item__numberItem" :class="{ '-active': n.trim() === '' }" :key="$index"
+                    v-else-if="cardNumber.length > $index">
                     {{ cardNumber[$index] }}
                   </div>
-                  <div
-                    class="card-item__numberItem"
-                    :class="{ '-active': n.trim() === '' }"
-                    v-else
-                    :key="$index + 1"
-                  >
+                  <div class="card-item__numberItem" :class="{ '-active': n.trim() === '' }" v-else :key="$index + 1">
                     {{ n }}
                   </div>
                 </transition>
@@ -116,22 +78,15 @@
               <transition name="slide-fade-up">
                 <div class="card-item__name" v-if="cardName.length" key="1">
                   <transition-group name="slide-fade-right">
-                    <span
-                      class="card-item__nameItem"
-                      v-for="(n, $index) in cardName.replace(/\s\s+/g, ' ')"
-                      v-if="$index === $index"
-                      v-bind:key="$index + 1"
-                      >{{ n }}</span
-                    >
+                    <span class="card-item__nameItem" v-for="(n, $index) in cardName.replace(/\s\s+/g, ' ')"
+                      v-if="$index === $index" v-bind:key="$index + 1">{{ n }}</span>
                   </transition-group>
                 </div>
                 <div class="card-item__name" v-else key="2">Full Name</div>
               </transition>
             </label>
             <div class="card-item__date" ref="cardDate">
-              <label for="cardMonth" class="card-item__dateTitle"
-                >Expires</label
-              >
+              <label for="cardMonth" class="card-item__dateTitle">Expires</label>
               <label for="cardMonth" class="card-item__dateItem">
                 <transition name="slide-fade-up">
                   <span v-if="cardMonth" v-bind:key="cardMonth">{{
@@ -155,14 +110,11 @@
       </div>
       <div class="card-item__side -back">
         <div class="card-item__cover">
-          <img
-            v-bind:src="
-              'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
-              currentCardBackground +
-              '.jpeg'
-            "
-            class="card-item__bg"
-          />
+          <img v-bind:src="
+            'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
+            currentCardBackground +
+            '.jpeg'
+          " class="card-item__bg" />
         </div>
         <div class="card-item__band"></div>
         <div class="card-item__cvv">
@@ -171,15 +123,11 @@
             <span v-for="(n, $index) in cardCVV" :key="$index"> * </span>
           </div>
           <div class="card-item__type">
-            <img
-              v-bind:src="
-                'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
-                getCardType +
-                '.png'
-              "
-              v-if="getCardType"
-              class="card-item__typeImg"
-            />
+            <img v-bind:src="
+              'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/' +
+              getCardType +
+              '.png'
+            " v-if="getCardType" class="card-item__typeImg" />
           </div>
         </div>
       </div>
@@ -188,72 +136,29 @@
   <div class="card-form__inner">
     <div class="card-input">
       <label for="cardNumber" class="card-input__label">Card Number</label>
-      <input
-        type="text"
-        id="cardNumber"
-        class="card-input__input"
-        v-mask="generateCardNumberMask"
-        v-model="cardNumber"
-        @input="updateCardNumber"
-        v-on:focus="focusInput"
-        v-on:blur="blurInput"
-        data-ref="cardNumber"
-        autocomplete="off"
-      />
+      <input type="text" id="cardNumber" class="card-input__input" v-mask="generateCardNumberMask" v-model="cardNumber"
+        @input="updateCardNumber" v-on:focus="focusInput" v-on:blur="blurInput" data-ref="cardNumber"
+        autocomplete="off" />
     </div>
     <div class="card-input">
       <label for="cardName" class="card-input__label">Card Holders</label>
-      <input
-        type="text"
-        id="cardName"
-        class="card-input__input"
-        v-model="cardName"
-        v-on:focus="focusInput"
-        v-on:blur="blurInput"
-        data-ref="cardName"
-        autocomplete="off"
-      />
+      <input type="text" id="cardName" class="card-input__input" v-model="cardName" v-on:focus="focusInput"
+        v-on:blur="blurInput" data-ref="cardName" autocomplete="off" />
     </div>
     <div class="card-form__row">
       <div class="card-form__col">
         <div class="card-form__group">
-          <label for="cardMonth" class="card-input__label"
-            >Expiration Date</label
-          >
-          <select
-            class="card-input__input -select"
-            id="cardMonth"
-            @input="updateCardMonth"
-            v-model="cardMonth"
-            v-on:focus="focusInput"
-            v-on:blur="blurInput"
-            data-ref="cardDate"
-          >
+          <label for="cardMonth" class="card-input__label">Expiration Date</label>
+          <select class="card-input__input -select" id="cardMonth" @input="updateCardMonth" v-model="cardMonth"
+            v-on:focus="focusInput" v-on:blur="blurInput" data-ref="cardDate">
             <option value="" disabled selected>Month</option>
-            <option
-              v-bind:value="n < 10 ? '0' + n : n"
-              v-for="n in 12"
-              v-bind:disabled="n < minCardMonth"
-              v-bind:key="n"
-            >
-              {{ n < 10 ? "0" + n : n }}
-            </option>
+            <option v-bind:value="n < 10 ? '0' + n : n" v-for="n in 12" v-bind:disabled="n < minCardMonth" v-bind:key="n">
+              {{ n < 10 ? "0" + n : n }} </option>
           </select>
-          <select
-            class="card-input__input -select"
-            id="cardYear"
-            v-model="cardYear"
-            @input="updateCardYear"
-            v-on:focus="focusInput"
-            v-on:blur="blurInput"
-            data-ref="cardDate"
-          >
+          <select class="card-input__input -select" id="cardYear" v-model="cardYear" @input="updateCardYear"
+            v-on:focus="focusInput" v-on:blur="blurInput" data-ref="cardDate">
             <option value="" disabled selected>Year</option>
-            <option
-              v-bind:value="$index + minCardYear"
-              v-for="(n, $index) in 12"
-              v-bind:key="n"
-            >
+            <option v-bind:value="$index + minCardYear" v-for="(n, $index) in 12" v-bind:key="n">
               {{ $index + minCardYear }}
             </option>
           </select>
@@ -262,18 +167,8 @@
       <div class="card-form__col -cvv">
         <div class="card-input">
           <label for="cardCvv" class="card-input__label">CVV</label>
-          <input
-            type="text"
-            class="card-input__input"
-            id="cardCvv"
-            v-mask="'####'"
-            maxlength="4"
-            v-model="cardCVV"
-            @input="updateCardCVV"
-            v-on:focus="flipCard(true)"
-            v-on:blur="flipCard(false)"
-            autocomplete="off"
-          />
+          <input type="text" class="card-input__input" id="cardCvv" v-mask="'####'" maxlength="4" v-model="cardCVV"
+            @input="updateCardCVV" v-on:focus="flipCard(true)" v-on:blur="flipCard(false)" autocomplete="off" />
         </div>
       </div>
     </div>
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,89 +1,106 @@
 <template>
-
-    <div class="overflow-auto flex flex-col chat-popup z-10 h-96  max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-80"
-        v-if="isOpen">
-        <div class="">
-            <input type="text" v-model="username" />
-            <li class="" v-for="message in messages" :key="message">
-                <div>{{ message.username }}</div>
-                <div class="">{{ message.message }}</div>
-            </li>
-        </div>
-        <div class="flex chat-input mt-auto items-center">
-            <input v-model="message" class="w-full  " type="text" placeholder="Send your message" />
-            <form @submit.prevent="submit">
-                <button type="submit" class="">
-                    <font-awesome-icon icon="fa-solid fa-paper-plane" />
+            <div class="chat-box overflow-auto flex flex-col chat-popup z-10 h-96 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-80"
+                v-if="isOpen" ref="chatBox">
+                <div class="">
+                    <input class="fixed top-0" type="text" v-model="username" />
+                    <li class="" v-for="message in messages" :key="message">
+                        <div>{{ message }}</div>
+                        <div class="">{{ message }}</div>
+                    </li>
+                </div>
+                <div class="chat-input mt-auto items-center flex">
+                    <input v-model="message" class="w-full" type="text" placeholder="Send your message" ref="chatInput" />
+                    <form @submit.prevent="submit">
+                        <button type="submit">
+                            <font-awesome-icon icon="fa-solid fa-paper-plane" />
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="chat-icon " @click="togglePopup">
+                <button>
+                    <font-awesome-icon class="fa-2xl" icon="fa-solid fa-comment " />
                 </button>
-            </form>
-        </div>
-
-
-    </div>
-    <div class="chat-icon" @click="togglePopup">
-        <button>
-            <font-awesome-icon class="fa-2xl" icon="fa-solid fa-comment " />
-        </button>
-    </div>
-
+            </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                isOpen: false,
-                messages: [],
-                message: "",
-                username: ""
-            };
-        },
-        mounted() {
-            var pusher = new Pusher('80e26ff66717be8fd4cf', {
-                cluster: 'mt1'
-            });
-
-            var channel = pusher.subscribe('chat');
-            channel.bind('message', (data) => {
-                this.messages.push(data)
-                console.log(this.messages)
-            });
-
-        },
-        methods: {
-            togglePopup() {
-                this.isOpen = !this.isOpen;
-            },
-            async submit() {
-                await fetch('http://localhost:8000/api/messages', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        username: this.username,
-                        message: this.message
-                    })
-                })
-                this.message = ''
-
+import Echo from "laravel-echo";
+import { HTTPS } from "../axios/http-axios"
+export default {
+    data() {
+        return {
+            isOpen: false,
+            messages: [],
+            message: "",
+            echo: null,
+            channel: null,
+            user: {
+                id: 1
             }
+        };
+    },
+    mounted() {
+        this.echo = new Echo({
+            broadcaster: 'pusher',
+            key: "80e26ff66717be8fd4cf",
+            cluster: "mt1",
+            encrypted: true,
+            authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth',
+            auth: {
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+                }
+            }
+        }),
+
+            this.channel = this.echo.private('user.' + this.user.id);
+
+        this.channel.listen('Message', (data) => {
+            this.messages.push(data.user.message);
+            console.log(data)
+        });
+    },
+    methods: {
+        scrollToBottom() {
+            const container = this.$el.querySelector('.chat-box');
+            container.scrollTop = container.scrollHeight;
         },
-    };
+        togglePopup() {
+            this.isOpen = !this.isOpen;
+        },
+        async submit() {
+            HTTPS.post('/messages', { message: this.message });
+            this.message = '';
+
+            this.$nextTick(() => {
+                const chatBox = this.$refs.chatBox;
+                chatBox.scrollTo({
+                    top: chatBox.scrollHeight,
+                    behavior: 'smooth'
+                });
+                this.$refs.chatInput.focus();
+            });
+
+        }
+    },
+};
 </script>
 
 <style scoped>
-    .chat-popup {
-        position: fixed;
-        bottom: 25%;
-        right: 20px;
+.chat-popup {
+    position: fixed;
+    bottom: 25%;
+    right: 20px;
 
-        /* Add any other styling you need for the chat popup */
-    }
+    /* Add any other styling you need for the chat popup */
+}
 
-    .chat-icon {
-        position: fixed;
+.chat-icon {
+    position: fixed;
 
-        bottom: 20%;
-        right: 20px;
-        /* Add any other styling you need for the chat icon */
-    }
+    bottom: 20%;
+    right: 20px;
+    /* Add any other styling you need for the chat icon */
+}
 </style>
