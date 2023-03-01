fc89a7d184a0636db8aa8ae3b2af6c7daf4b107a
kaydenma2002 <113602175+kaydenma2002@users.noreply.github.com>
2023-03-01 16:38:55 -0600

    commit

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-2xl border rounded chat-box overflow-auto flex flex-col chat-popup z-10  max-w-sm  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+    <div class="w-96  border rounded chat-box overflow-auto flex flex-col chat-popup z-10    bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
         v-if="isOpen">
         <div class="w-full ">
             <div class="sticky top-0 bg-white z-40">
@@ -11,7 +11,7 @@
                     </span>
                 </div>
             </div>
-            <div class=" w-full h-96 p-6 overflow-y-auto " ref="chatContainer">
+            <div class=" w-full h-106 p-6 overflow-y-auto " ref="chatContainer">
                 <ul v-for="(item, index) in sent" class="space-y-2" ref="scrollToMe">
 
                     <li class="flex justify-end">
@@ -19,14 +19,122 @@
                             <span class="block">{{ item }}</span>
                         </div>
                     </li>
-                    <li class="flex justify-start">
+                    <li v-if="sent[index].includes('reservation')">
+                        <form @submit.prevent="submitReservation">
+                            <VueDatePicker @open="datePickerFocus" placeholder="Date of reservation" v-model="date">
+                            </VueDatePicker>
+                            <div v-for="error of v$.date.$errors" :key="error">
+                                <p>{{ error.$message }}</p>
+                            </div>
+                            <select id="table" v-model="table"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected>Number of table </option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4"> >4 </option>
+                            </select>
+                            <div v-for="error of v$.table.$errors" :key="error">
+                                <p>{{ error.$message }}</p>
+                            </div>
+                            <form @submit.prevent="verifyPhoneNumber">
+                                <div class="relative">
+                                    <input type="number" v-model="phone" :disabled="verifyPhone"
+                                        class="block w-full p-4  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Enter your mobile number">
+                                    <button v-if="!verifyPhone" type="submit"
+                                        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Verify</button>
+                                </div>
+                                <div v-for="error of v$.phone.$errors" :key="error">
+                                    <p>{{ error.$message }}</p>
+                                </div>
+                            </form>
+                            <input v-if="verifyPhone" type="number" v-model="verify_code" id="helper-text"
+                                aria-describedby="helper-text-explanation"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Enter your verify code send to the number above">
+                            <div v-for="error of v$.verify_code.$errors" :key="error">
+                                <p>{{ error.$message }}</p>
+                            </div>
+                            <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">We’ll
+                                never
+                                share your details. Read our <a href="#"
+                                    class="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy Policy</a>.
+                            </p>
+                            <div v-if="verifyPhone" class="w-full text-right ">
+                                <button type="submit"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+                            </div>
+
+                        </form>
+                    </li>
+                    <li v-else-if="sent[index].includes('contact')" class="flex justify-start">
+                        Please contact us at 571.505.1131
+                    </li>
+                    <li v-else-if="sent[index].includes('order')">
+                        <form @submit.prevent="submitRestaurant">
+                            <div class="relative">
+                                <input type="text" v-model="restaurant" :disabled="search_restaurant"
+                                    class="block w-full p-4  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Enter restaurant">
+                                <button type="submit"
+                                    class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                            </div>
+                            <div v-if="search_restaurant" class="grid grid-cols-2 gap-4">
+                                <div v-for="(item, index) in items" :key="index" class="box-content ...">
+                                    <div class="overlay">
+                                        <FoodCardForChat :id="item.id" :name="item.name" :description="item.description"
+                                            :price="item.price" :image="item.image" />
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </li>
+                    <li v-else class="flex justify-start">
                         <div class=" max-w-xl px-4 py-2 text-gray-700 rounded shadow">
                             <span class="block">{{ reply[index] }}</span>
                         </div>
                     </li>
 
                 </ul>
+            </div>
+            <div class="">
+                <div class="text-center w-full">
+                    What may I help you today ?
+                </div>
+                <div class="flex justify-between">
+                    <button type="button" @click="submitbyChoices('order')"
+                        class="
+                                                                                                                                                                                                                                                                                        bg-indigo-600
+                                                                                                                                                                                                                                                                                        px-2
+                                                                                                                                                                                                                                                                                        py-2
+                                                                                                                                                                                                                                                                                        rounded-lg
+                                                                                                                                                                                                                                                                                        text-white
+                                                                                                                                                                                                                                                                                        hover:bg-indigo-500
+                                                                                                                                                                                                                                                                                        text-sm
+                                                                                                                                                                                                                                                                                        shadow-lg shadow-indigo-500/50
+                        
+                                                                                                                                                                                                                                                                                    ">
+                        Online Order
+                    </button>
+                    <button type="button" @click="submitbyChoices('contact')" class="
+                                                                                                                    bg-indigo-600
+                                                                                                                    px-2
+                                                                                                                    py-2
+                                                                                                                    rounded-lg
+                                                                                                                    text-white
+                                                                                                                    hover:bg-indigo-500
+                                                                                                                    text-sm
+                                                                                                                    shadow-lg shadow-indigo-500/50
 
+                                                                                                                    ">
+                        Contact Us
+                    </button>
+                    <button type="button" @click="submitbyChoices('reservation')"
+                        class="bg-indigo-600 px-2 py-2 rounded-lg text-white hover:bg-indigo-500 text-sm shadow-lg shadow-indigo-500/50                                                             ">
+                        Make a Reservation
+                    </button>
+                </div>
             </div>
             <form class="sticky bottom-0  z-10 bg-white" @submit.prevent="submit">
                 <div class="flex items-center justify-between w-full p-3 border-t border-gray-300">
@@ -76,11 +184,26 @@
 </template>
 
 <script>
+import FoodCardForChat from "../components/FoodCardForChat.vue";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import useValidate from "@vuelidate/core";
+import {
+    required,
+    between,
+    email,
+    numeric,
+    minLength,
+} from "@vuelidate/validators";
+import '@vuepic/vue-datepicker/dist/main.css'
+
+import Swal from 'sweetalert2'
 import Echo from "laravel-echo";
-import { HTTPS } from "../axios/http-axios"
+import { HTTP, HTTPS } from "../axios/http-axios"
 export default {
+    components: { VueDatePicker, FoodCardForChat },
     data() {
         return {
+            v$: useValidate(),
             loading: false,
             isOpen: false,
             sent: [],
@@ -88,8 +211,26 @@ export default {
             message: "",
             echo: null,
             channel: null,
-            user: ""
+            user: "",
+            phone: null,
+            verify_code: null,
+            table: null,
+            date: null,
+            verifyPhone: false,
+            phone_error: null,
+            search_restaurant: false,
+            restaurant: null,
+            items: [],
+            restaurant: null
         };
+    },
+    validations() {
+        return {
+            phone: { required },
+            date: { required },
+            verify_code: { required },
+            table: { required }
+        }
     },
     updated() {
         this.$nextTick(() => {
@@ -97,12 +238,6 @@ export default {
         });
     },
     mounted() {
-        HTTPS.get('/user/chat').then(res => {
-            for (var i = 0; i < res.data.length; i++) {
-                this.sent.push(res.data[i].message)
-                this.reply.push(res.data[i].reply)
-            }
-        })
         HTTPS.get('/user').then(res => {
             this.user = res.data.id
             this.echo = new Echo({
@@ -120,18 +255,87 @@ export default {
 
                 this.channel = this.echo.private('user.' + this.user);
             this.channel.listen('Message', (data) => {
+
                 this.reply.push(data.chat.reply)
+
             });
         })
 
 
     },
     methods: {
+        datePickerFocus() {
+            this.$nextTick(() => {
+                this.scrollToBottom();
+            });
+        },
+        verifyPhoneNumber() {
+            this.v$.phone.$touch();
+            if(!this.v$.phone.$invalid) {
+                this.verifyPhone = true
+                HTTP.post('/verify-mobile', {
+                    phone: this.phone,
+                    verify_code: this.verify_code
+                }).then(res => {
+                    console.log(res)
+                })
+            } else {
+                this.phone_error = true
+            }
+
+        },
+        submitRestaurant() {
+            HTTP.get('/submit-restaurant', {
+                params: { restaurant: this.restaurant }
+            }).then(res => {
+                this.search_restaurant = true
+                for(var i = 0; i < res.data.item.length; i++) {
+                    this.items.push(res.data.item[i])
+                }
+            }).catch((error => {
+                console.log(error)
+            }))
+
+        },
         togglePopup() {
             this.isOpen = !this.isOpen;
         }, scrollToBottom() {
             const chatContainer = this.$refs.chatContainer;
             chatContainer.scrollTop = chatContainer.scrollHeight;
+        },
+        async submitReservation() {
+            Swal.fire({
+                title: `please confirm your new reservation: ${this.table} tables at ${this.date}`,
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'Confirm',
+                denyButtonText: `Don't save`,
+            }).then((result) => {
+
+                if(result.isConfirmed) {
+                    this.v$.$validate();
+                    if(!this.v$.$error) {
+                        HTTPS.post('/submit-reservation', {
+                            phone: this.phone,
+                            verify_code: this.verify_code,
+                            table: this.table,
+                            date: this.date
+                        }).then((res) => {
+                            if(res.data.success != false) {
+                                this.verifyPhone = true
+                                Swal.fire('Your reservation has been confirmed', '', 'success')
+                            } else {
+                                console.log(res)
+                            }
+                        }).catch((error) => {
+                            console.log((error))
+                        })
+                    }
+                } else if(result.isDenied) {
+                    Swal.fire('Changes are not saved', '', 'info')
+                }
+            })
+
         },
         async submit() {
             this.loading = true
@@ -143,6 +347,40 @@ export default {
             HTTPS.post('/messages', { message: this.message }).then(res => {
                 this.loading = false
             });
+
+            this.message = '';
+        },
+        async submitbyChoices(e) {
+            this.message = e
+            
+            if(this.message === 'reservation') {
+                this.phone = null
+                this.verify_code = null
+                this.table = null
+                this.date = null
+                this.verifyPhone = false
+                this.phone_error = null
+            } else if(this.message === 'order') {
+                this.search_restaurant = false
+                this.restaurant = null
+                this.items = []
+                this.restaurant = null
+            }
+            this.sent.push(this.message)
+            console.log(this.sent)
+            if(this.sent.filter(item => item.includes('reservation')).length > 1) {
+                this.sent.splice(this.sent.indexOf('reservation'), 1)
+            } else if(this.sent.filter(item => item.includes('order')).length > 1) {
+                this.sent.splice(this.sent.indexOf('order'), 1)
+            }
+            
+            this.$nextTick(() => {
+                this.scrollToBottom();
+            });
+            HTTPS.post('/messages', { message: this.message }).then(res => {
+                this.loading = false
+
+            });
             this.message = '';
         },
 
@@ -153,16 +391,24 @@ export default {
 <style scoped>
 .chat-popup {
     position: fixed;
-    bottom: 25%;
+    bottom: 15%;
     right: 20px;
 
     /* Add any other styling you need for the chat popup */
 }
 
+.h-106 {
+    height: 30rem;
+}
+
+.w-112 {
+    width: 32rem
+}
+
 .chat-icon {
     position: fixed;
 
-    bottom: 20%;
+    bottom: 10%;
     right: 20px;
     /* Add any other styling you need for the chat icon */
 }
---
+++ src/components/FoodCardForChat.vue
@@ -0,0 +1,59 @@
+<template>
+    <div href="#" class="
+              flex flex-col
+              items-center
+              bg-white
+              shadow-md
+              md:flex-row md:max-w-xl
+              h12
+              hover:bg-gray-100
+            ">
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+            <img class="rounded-t-lg" v-bind:src="image" alt="" />
+
+            <div class="p-5">
+
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ name }} </h5>
+
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ description }}</p>
+                <p class="text-sm text-gray-700 dark:text-gray-400">${{ price }}</p>
+                <div class="ml-auto">
+                    <button v-if="enableAddToCart" @click="addToCart(id)">
+                        <font-awesome-icon icon="fa-solid fa-plus" />
+                    </button>
+                    <button v-if="enableRemoveFromCart" @click="RemoveFromCart(id)">
+                        <font-awesome-icon icon="fa-solid fa-minus" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Swal from "sweetalert2";
+import { numeric } from "@vuelidate/validators";
+import { HTTPS } from "../axios/http-axios"
+export default {
+    props: {
+        id: String,
+        name: String,
+        category: String,
+        price: String,
+        description: String,
+        image: String,
+        imageOnClick: Function,
+        editContent: Function,
+        enableAddToCart: {
+            type: Boolean,
+            default: true
+        },
+        enableRemoveFromCart: {
+            type: Boolean,
+            default: false
+        }
+
+    },
+
+};
+</script>
